b36edc50819f145083001c628c6aa91d178de217
ramiinberg <rami.inberg@gmail.com>
2022-07-07 11:10:30 +0300

    added one more category

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 async function fetchCategories(count, offset) {
-  const response = await fetch(`https://jservice.io/api/categories?count=${count}`)
+  const response = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`)
   const data = await response.json()
   return data
 }
 
+function getClueHtml(clueValue) {
+  return `<div class="my-category-clue" style="grid-row-start: ${clueValue / 100 + 1}">$${clueValue}</div>`
+}
+
+
 function getCategoryHtml(category) {
   return `
   <div class="my-category-title">${category.title}</div>
-
-  <div class="my-category-clue" style="grid-row-start: 2">$100</div>
-  <div class="my-category-clue" style="grid-row-start: 3">$200</div>
-  <div class="my-category-clue" style="grid-row-start: 4">$300</div>
-  <div class="my-category-clue" style="grid-row-start: 5">$400</div>
+  ${getClueHtml(100)}
+  ${getClueHtml(200)}
+  ${getClueHtml(300)}
+  ${getClueHtml(400)}
   `
 }
 
-fetchCategories(4, 0).then(categories => {
+fetchCategories(5, 10).then(categories => {
   const categoryNamesHtml = categories.map(getCategoryHtml).join("")
   document.getElementById("jeopardy").innerHTML = categoryNamesHtml
   console.log('data', categories)
